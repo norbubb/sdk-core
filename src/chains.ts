eb88d9ffb62f1b92862b0e5f5bd8d3e8c81ef614
norbubb <norbubb@proton.me>
2024-04-01 20:27:25 +0800

    init

--- src/chains.ts
+++ src/chains.ts
@@ -21,7 +21,8 @@ export enum ChainId {
   ZORA = 7777777,
   ZORA_SEPOLIA = 999999999,
   ROOTSTOCK = 30,
-  BLAST = 81457
+  BLAST = 81457,
+  X1_TESTNET = 195,
 }
 
 export const SUPPORTED_CHAINS = [
@@ -45,7 +46,8 @@ export const SUPPORTED_CHAINS = [
   ChainId.ZORA,
   ChainId.ZORA_SEPOLIA,
   ChainId.ROOTSTOCK,
-  ChainId.BLAST
+  ChainId.BLAST,
+  ChainId.X1_TESTNET,
 ] as const
 export type SupportedChainsType = typeof SUPPORTED_CHAINS[number]
 
@@ -58,5 +60,6 @@ export enum NativeCurrencyName {
   MOONBEAM = 'GLMR',
   BNB = 'BNB',
   AVAX = 'AVAX',
-  ROOTSTOCK = 'RBTC'
+  ROOTSTOCK = 'RBTC',
+  OKB = 'OKB'
 }
