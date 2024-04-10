dde56fe31971db9fc35eb7dd2104c8d4877b7293
norbubb <norbubb@proton.me>
2024-04-10 14:45:20 +0800

    reinit

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -67,5 +67,6 @@ export const WETH9: { [chainId in ChainId]: Token } = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH9', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH9', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH9', 'Wrapped Ether'),
+  [ChainId.X1_TESTNET]: new Token(ChainId.X1_TESTNET, '0xEE1a9629cce8f26deB1EcFfBd8F306bef2117423', 18, 'WOKB', 'Wrapped OKB')
 }
